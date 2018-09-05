2f19d494bdec3c0dd54c4ee93b90cd45d9bcc6a3
hlockward <hlockward12@gmail.com>
2018-09-05 08:07:25 -0400

    Assignment 1 completed

--- src/component/DishDetailsComponent.js
+++ src/component/DishDetailsComponent.js
@@ -2,26 +2,31 @@ import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
 const DishDetails = ({dish}) =>{
+    
     return(
         <div className="row">
-            <Card className="col-12 col-md-6">
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-            <Card className="col-12 col-md-6">
-                <CardBody>
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <ul className="list-unstyled">
+                    <li><h3>{dish.comments.length > 0 ? 'Comments' : ''}</h3></li>
                     {dish.comments.map(comment =>
                         <div key={comment.id}>
-                            <CardText>{comment.comment}</CardText>
-                            <CardText>{ `-- ${comment.author} , ${comment.date}`}</CardText>
+                            <li>{comment.comment}</li>
+                            <br/>
+                            <li>{ `-- ${comment.author} , ${comment.date}`}</li>
+                            <br/>
                         </div>
                     )}
-                    
-                </CardBody>
-            </Card>
+                </ul>
+            </div>
         </div>
         
 
--- src/component/MenuComponent.js
+++ src/component/MenuComponent.js
@@ -45,11 +45,7 @@ class Menu extends Component{
                 <div className='row'>
                     {menu}
                 </div>
-                <div className='row'>
-                    <div className='col-12 col-md-10 m-1'>
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                </div>
+                {this.renderDish(this.state.selectedDish)}
             </div>
         );
     }
