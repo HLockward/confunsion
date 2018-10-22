23e1de4d47d97873c4c2b729194fb994b0752111
Harold Lockward <haroldlockward@Harolds-MacBook-Pro.local>
2018-10-21 23:05:02 -0400

    first task 4 assignment

--- src/component/AboutComponent.js
+++ src/component/AboutComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
 
 const RenderLeader = ({leader}) =>{
     return(
         <Media tag="li">
             <Media left middle>
-                <Media object src={leader.image} alt={leader.name} />
+                <Media object src={baseUrl + leader.image} alt={leader.name} />
             </Media>
             <Media body className="ml-5">
             <Media className="mb-2" heading>{leader.name}</Media>
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -144,3 +144,40 @@ export const promosFailed = (errmess) => ({
     type: ActionTypes.PROMOS_FAILED,
     payload: errmess
 });
+
+/** LEADERS */
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading());
+
+    return fetch(baseUrl + 'leaders')
+        .then(response => {
+            if(response.ok){
+                return response;
+            }else{
+                var error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        }, error =>{
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)));
+};
+
+export const leadersLoading = () =>({
+    type: ActionTypes.LEADERS_LOADING
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
+});
+
+export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errmess
+});
