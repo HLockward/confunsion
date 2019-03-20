2da9c7700ebb631b79b7220a2d7fea7ac813346e
Harold Lockward <haroldlockward@Harolds-MacBook-Pro.local>
2019-03-19 20:45:29 -0400

    uploaded to gh page

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'https://localhost:3444/';
+export const baseUrl = (process.env.NODE_ENV !== 'production' ? 'https://localhost:3444/' : 'https://confunsionserver.herokuapp.com/');
+export const HOME_URL = (process.env.NODE_ENV !== 'production' ? '/' : 'https://hlockward.github.io/confunsion/');
